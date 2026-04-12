ef8b4b21ca556a0026357599ee2744105863018f
Aryan Saxena <aryansaxena1210@gmail.com>
2026-04-12 17:17:26 -0500

    add RSFS_init functionality

--- api.c
+++ api.c
@@ -10,8 +10,53 @@ pthread_mutex_t mutex_for_fs_stat;//mutex used by RSFS_stat()
 //initialize file system - should be called as the first thing before accessing this file system 
 int RSFS_init(){
 
-//------ your implementation -------
+    pthread_mutex_init(&mutex_for_fs_stat, NULL);
+    pthread_mutex_init(&inode_bitmap_mutex, NULL);
+    pthread_mutex_init(&inodes_mutex, NULL);
+    pthread_mutex_init(&data_bitmap_mutex, NULL);
+    pthread_mutex_init(&root_dir_mutex, NULL);
+    pthread_mutex_init(&open_file_table_mutex, NULL);
 
+    // initialize inode bitmap and inodes
+    for(int i=0; i<NUM_INODES; i++){
+        inode_bitmap[i] = 0;
+    }
+
+    for(int i=0; i<NUM_INODES; i++){
+        for(int j=0; j<NUM_POINTERS; j++){
+            inodes[i].block[j] = -1;
+        }
+        inodes[i].length = 0;
+    }
+
+    // initialize data bitmap and data blocks
+    for(int i=0; i<NUM_DBLOCKS; i++){
+        data_bitmap[i] = 0;
+    }
+
+    for(int i=0; i<NUM_DBLOCKS; i++){
+        data_blocks[i] = malloc(BLOCK_SIZE);
+        if(data_blocks[i] == NULL){
+            printf("[RSFS_init] fail to allocate data block %d.\n", i);
+            return -1;
+        }
+    }
+
+    // initialize open file table
+    for(int i=0; i<NUM_OPEN_FILE; i++){
+        open_file_table[i].used = 0;
+        open_file_table[i].position = 0;
+    }
+
+    // allocate inode for root directory
+    root_inode_number = allocate_inode(); //look inside bitmap, find first free location, initialize inode with that id, set bitmap of that lco to 1, return id.
+    //to access the node again, do inodes[root_inode_number], and side note: bitmap[root_inode_number] if ofc ==1
+    if(root_inode_number < 0){
+        printf("[RSFS_init] fail to allocate root inode.\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 
