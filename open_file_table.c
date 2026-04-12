357d02f4adf10898077aaf103c9acd602cc1f8f1
Aryan Saxena <aryansaxena1210@gmail.com>
2026-04-12 16:15:47 -0500

    add file table functionality

--- open_file_table.c
+++ open_file_table.c
@@ -8,18 +8,35 @@
 struct open_file_entry open_file_table[NUM_OPEN_FILE];
 pthread_mutex_t open_file_table_mutex;
 
-
 //allocate an available entry in open file table and return fd (file descriptor);
 //return -1 if no entry is found
 int allocate_open_file_entry(int access_flag, int inode_number){
-    
-//------ your implementation -------
+    int fd = -1;
 
+    pthread_mutex_lock(&open_file_table_mutex);
+
+    for(int i=0; i<NUM_OPEN_FILE; i++){
+        if(open_file_table[i].used == 0){
+            fd = i;
+            open_file_table[i].used = 1;
+            open_file_table[i].access_flag = access_flag;
+            open_file_table[i].inode_number = inode_number;
+            open_file_table[i].position = 0;
+            (&open_file_table[i].entry_mutex, NULL);
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&open_file_table_mutex);
+
+    return fd;
 }
 
 
-void free_open_file_entry(int fd){
-
-//------ your implementation -------
+void free_open_file_entry(int fd)
+{
+    pthread_mutex_lock(&open_file_table_mutex);
+    open_file_table[fd].used = 0;
+    pthread_mutex_unlock(&open_file_table_mutex);
 
 }
